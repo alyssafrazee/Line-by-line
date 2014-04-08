49f3167c24da42315cd29dee19fa8474d59afc3c
Alyssa Frazee <acfrazee@gmail.com>
2014-04-08 18:11:13 -0400

    adds functionality for automatic roxygen docstring templates in R! code from @karthik, RTools repo

--- line-by-line.py
+++ line-by-line.py
@@ -86,6 +86,23 @@ class SendSelectCommand(sublime_plugin.TextCommand):
             cmd += thiscmd +'\n'
         runcmd(cmd)
 
+class RDocsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sel = self.view.sel()[0]
+
+        params_reg = self.view.find('(?<=\().*(?=\))', sel.begin())
+        params_txt = self.view.substr(params_reg)
+        params = params_txt.split(',')
+
+        snippet = "#'<brief desc>\n#'\n#'<full description>\n"
+
+        for p in params:
+            snippet += "#' @param %s <what param does>\n" % p
+
+        snippet += "#' @export\n#' @keywords\n#' @seealso\n#' @return\n#' @alias\n#' @examples \dontrun{\n#'\n#'}\n"
+
+        self.view.insert(edit, sel.begin(), snippet)
+
 
 class AppSwitcher(sublime_plugin.WindowCommand):
 
